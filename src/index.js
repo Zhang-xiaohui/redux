706a7d28033e4e0647b02a7eec419a8b9d1b48a7
ZH <I_amhuihui@163.com>
2018-04-11 20:50:37 +0800

    initial project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import ReactDom from "react-dom";
+import {view as Todos} from "./todos/";
+import {view as Filter} from "./filter/";
+import {Provider} from 'react-redux';
+import store from './store.js';
+
+import './css/page.css'
+
+class TodoApp extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<div>
+					<Todos />
+					<Filter />
+				</div>
+			</Provider>
+		)
+	}
+}
+
+ReactDom.render(<TodoApp/>, document.getElementById("root"));
